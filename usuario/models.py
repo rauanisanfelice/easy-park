41fd6d5942c46ee8d675cf8ce96c6ae5fcef4370
Rauan <rauan.sanfelice@gmail.com>
2019-12-06 16:47:46 -0300

    Criando tabelas nos models

--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class infoUsuario(models.Model):
+    db_table = 'infoUsuario'
+
+    cpf = models.CharField(max_length=100)
+    cidade =  models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+
+class funcionario(models.Model):
+    db_table = 'funcionario'
+
+    setor = models.CharField(max_length=20)
+    fiscal = models.CharField(max_length=20)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+class carteira(models.Model):
+    db_table = 'carteira'
+
+    valor = models.DecimalField(max_digits=15, decimal_places=2)
+    saldo = models.DecimalField(max_digits=15, decimal_places=2)
+
+    cho_status = [
+        ('en', 'entrada'),
+        ('sa', 'saida'),
+    ]
+    tipo_lancamento = models.CharField(choices=cho_status, max_length=2, default=cho_status[0][0])
+    data_insercao = models.DateTimeField(auto_now_add=True)
+
+class veiculo(models.Model):
+    db_table = 'veiculo'
+
+    placa = models.CharField(max_length=7)
+    modelo = models.CharField(max_length=100)
+
+
+class parada(models.Model):
+    db_table = 'parada'
+    
+    veiculos = models.ForeignKey(veiculo, on_delete=models.PROTECT)
+    data_parada = models.DateTimeField(auto_now_add=True)
+    hora_parada = models.TimeField(auto_now_add=True)
+    
+    cho_quantidade_horas = [
+        ('1h', 'uma hora'),
+        ('1h:30min', 'uma hora e trinta minutos'),
+        ('2h', 'duas horas'),
+        ('2h:30min', 'duas horas e trinta minutos'),
+        ('3h', 'três hora'),
+        ('3h:30min', 'três horas e trinta minutos'),
+        ('4h', 'quatro hora'),
+    ]
+    quantidade_horas = models.CharField(choices=cho_quantidade_horas, max_length=8, default=cho_quantidade_horas[0][0])
+
+class notificacao(models.Model):
+    db_table = 'notificacao'
+    verbose_name_plural = 'notificacoes'
+
+    paradas = models.ForeignKey(parada, on_delete=models.PROTECT)
+    funcionario = models.ForeignKey(User, on_delete=models.PROTECT)
+    data_notificacao = models.TimeField(auto_now_add=True)
+    descricao_notificao =models.CharField(max_length=100)
+    data_lida = models.TimeField(null=True)
--- vendedor/models.py
+++ vendedor/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from usuario.models import User, parada
+
+class pesquisaVeiculo(models.Model):
+    db_table = 'pesquisaVeiculo'
+    verbose_name_plural = 'pesquisasVeiculos'
+
+    funcionario = models.ForeignKey(User, on_delete=models.PROTECT)
+    paradas = models.ForeignKey(parada, on_delete=models.PROTECT)
+    data_pesquisa = models.TimeField(auto_now_add=True)
