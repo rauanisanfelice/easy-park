1b279604ed0e5c12fe4dbe5518b30a74e96415fd
Rauan <rauan.sanfelice@gmail.com>
2019-12-06 16:49:42 -0300

    Criando pagina template

--- usuario/views.py
+++ usuario/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+class index(View):
+    retorno = 'index.html'
+    def get(self, request):
+        return render(request, self.retorno)
