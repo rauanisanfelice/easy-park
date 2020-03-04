dc966dce30d17614516158f204cc7441527649ed
rauanisanfelice <rauan.sanfelice@gmail.com>
2020-03-04 15:45:54 -0300

    Criando paginas Notificacoes / Info / Perfil

--- usuario/views.py
+++ usuario/views.py
@@ -1,28 +1,46 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
 
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
 
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import *
 
+
+
 def index(request):
     return redirect('login')
+
 
 class Home(View):
     retorno = 'home.html'
     def get(self, request):
         return render(request, self.retorno)
 
+
 class SignUp(generic.CreateView):
     form_class = SignUpForm
     success_url = reverse_lazy('home')
     template_name = 'signup.html'
 
+
 class Perfil(View):
-    retorno = 'blank.html'
+    retorno = 'perfil.html'
+    def get(self, request):
+        return render(request, self.retorno)
+
+
+class Informacoes(View):
+    retorno = 'info.html'
+    def get(self, request):
+        return render(request, self.retorno)
+
+
+class Notificacoes(View):
+    retorno = 'notificacoes.html'
     def get(self, request):
         return render(request, self.retorno)
