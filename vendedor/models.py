04ca6b901999cb7127a47bd046d1bc1746dcacca
rauanisanfelice <rauan.sanfelice@gmail.com>
2020-03-10 16:21:50 -0300

    Ajuste nomenclatura

--- vendedor/models.py
+++ vendedor/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-from usuario.models import User, parada
+from usuario.models import User, Parada
 
 class pesquisaVeiculo(models.Model):
     db_table = 'pesquisaVeiculo'
     verbose_name_plural = 'pesquisasVeiculos'
 
     funcionario = models.ForeignKey(User, on_delete=models.PROTECT)
-    paradas = models.ForeignKey(parada, on_delete=models.PROTECT)
+    paradas = models.ForeignKey(Parada, on_delete=models.PROTECT)
     data_pesquisa = models.TimeField(auto_now_add=True)
