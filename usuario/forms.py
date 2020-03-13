02256fed05ccb9d610da41dce1d0ad519a20d128
rauanisanfelice <rauan.sanfelice@gmail.com>
2020-03-13 11:46:17 -0300

    Incluindo nome no cadastro

--- usuario/forms.py
+++ usuario/forms.py
@@ -7,4 +7,4 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', )
+        fields = ('first_name', 'username', 'email', 'password1', 'password2', )
