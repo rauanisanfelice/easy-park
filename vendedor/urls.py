d2cf5cf05c338ed05478bd7f92fe49838b427b32
rauanisanfelice <rauan.sanfelice@gmail.com>
2020-04-18 10:41:23 -0300

    Incluindo tela de verificacao de veiculo (incompleto)

--- vendedor/urls.py
+++ vendedor/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
-from .views import *
+from .views import VerificaVeiculo
 
 urlpatterns = [
-    
+    path('', VerificaVeiculo.as_view(), name='vendedor_verifica'),
 ]
--- vendedor/views.py
+++ vendedor/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+from django.contrib.admin.views.decorators import staff_member_required
+
+import logging
+import datetime
+
+logger = logging.getLogger(__name__)
+
+class VerificaVeiculo(View):
+    retorno = 'verifica.html'
+    
+    # @staff_member_required
+    def get(self, request):
+        return render(request, self.retorno)
