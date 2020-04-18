29e91db36122244a7ae530c6e1751f7034cb8280
rauanisanfelice <rauan.sanfelice@gmail.com>
2020-04-18 11:53:38 -0300

    Ajustando tabela de busca do vendedor

--- vendedor/models.py
+++ vendedor/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-from usuario.models import User, Parada
+from usuario.models import User, Parada, Veiculo
 
-class pesquisaVeiculo(models.Model):
+class PesquisaVeiculo(models.Model):
     db_table = 'pesquisaVeiculo'
     verbose_name_plural = 'pesquisasVeiculos'
 
     funcionario = models.ForeignKey(User, on_delete=models.PROTECT)
-    paradas = models.ForeignKey(Parada, on_delete=models.PROTECT)
+    veiculo = models.ForeignKey(Veiculo, on_delete=models.PROTECT, null=True)
+    placa_pesquisa = models.CharField(max_length=10)
     data_pesquisa = models.TimeField(auto_now_add=True)
