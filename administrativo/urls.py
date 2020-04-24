1e519bbb65260e07716ea304a8c3c73931f0a202
rauanisanfelice <rauan.sanfelice@gmail.com>
2020-04-24 15:41:48 -0300

    Incluindo keepalive e telas de historico e report

--- administrativo/urls.py
+++ administrativo/urls.py
@@ -5,5 +5,9 @@ from django.contrib.auth import views as auth_views
 from .views import *
 
 urlpatterns = [
+    path('keepalive/', keepalive, name='keepalive'),
+
     path('', Dashboard.as_view(), name='dashboard'),
+    path('historico/', Historico.as_view(), name='historico'),
+    path('report/', Report.as_view(), name='report'),
 ]
