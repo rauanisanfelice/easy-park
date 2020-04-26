0560c7372cae8267ff7b7a00d0746eb5efbab69b
rauanisanfelice <rauan.sanfelice@gmail.com>
2020-04-26 10:39:46 -0300

    ajustando nome da url historico admin

--- administrativo/urls.py
+++ administrativo/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('keepalive/', keepalive, name='keepalive'),
 
     path('', Dashboard.as_view(), name='dashboard'),
-    path('historico/', Historico.as_view(), name='historico'),
+    path('historico/', Historico.as_view(), name='historico-admin'),
     path('report/', Report.as_view(), name='report'),
 ]
