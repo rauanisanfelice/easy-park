6020fe4080531b5c2184e00976dcfe40fc18c801
rauanisanfelice <rauan.sanfelice@gmail.com>
2020-04-29 09:02:32 -0300

    Ajustando retornos context

--- vendedor/views.py
+++ vendedor/views.py
@@ -13,21 +13,21 @@ import datetime
 logger = logging.getLogger(__name__)
 
 class VerificaVeiculo(View):
-    retorno = 'verifica.html'
+    template_name = 'verifica.html'
     
     def get(self, request):
-        return render(request, self.retorno)
+        return render(request, self.template_name)
 
     def post(self, request):
         placa = request.POST.get('placa')
         pesquisa = PesquisaVeiculo(funcionario=request.user,placa_pesquisa=placa)
+        context = {}
         if not placa:
             pesquisa.save()
-            return render(request, self.retorno, {
-                "error": "True",
-                "error_mensagem": "Campos vazios.",
-                "acao": ""
-            })
+
+            context['error'] = True
+            context['error_mensagem'] = "Campos vazios."
+            context['acao'] = ""
         else:
             veiculo = Veiculo.objects.filter(placa=placa, ativo=True)[:1]
             try: # verifica se possui veiculo cadastrado
@@ -39,49 +39,53 @@ class VerificaVeiculo(View):
             pesquisa.veiculo = veiculo
             pesquisa.save()
 
-            if not veiculo:
+            if veiculo:
+                if veiculo.user:
+                    if check_veiculo_horario(request, None, veiculo):
+                        tipo_not = TipoNotificacao.objects.get(id=3)
+                        notificacao = Notificacao(parada=None, tipo_notificacao=tipo_not, user=veiculo.user)
+                        notificacao.save()
+
+                        context['error'] = True
+                        context['error_mensagem'] = "Veículo não realizou uma parada."
+                        context['acao'] = "Já realizamos a notificação via aplicativo!"
+                        context['aplicativo'] = "aplicativo"
+                    else:
+                        context['sucesso'] = True
+                        context['sucesso_mensagem'] = "Veículo possui uma parada."
+                else:
+                    context['error'] = True
+                    context['error_mensagem'] = "Veículo não esta cadastrado."
+                    context['acao'] = "Realizar notificação via talão!"
+            else:
+                # VEICULO NÃO CADASTRADO
                 try:
                     tipo_not = TipoNotificacao.objects.get(id=4)
                     notificacao = Notificacao(parada=None, tipo_notificacao=tipo_not, placa=placa)
                     notificacao.save()
                 except:
                     logger.error(f'Erro ao noticar veiculo nao cadastrado - Placa ({placa}) - Id Funcionario ({request.user.id})')
-                return render(request, self.retorno, {
-                    "error": "True",
-                    "error_mensagem": "Veículo não esta cadastrado.",
-                    "acao": "Realizar notificação via talão!",
-                })
-            else:        
-                if check_veiculo_horario(request, None, veiculo):
-                    tipo_not = TipoNotificacao.objects.get(id=3)
-                    notificacao = Notificacao(parada=None, tipo_notificacao=tipo_not, user=veiculo.user)
-                    notificacao.save()
-                    return render(request, self.retorno, {
-                        "error": "True",
-                        "error_mensagem": "Veículo não realizou uma parada.",
-                        "acao": "Já realizamos a notificação via aplicativo!",
-                    })
-                else:
-                    return render(request, self.retorno, {
-                        "sucesso": "True",
-                        "sucesso_mensagem": "Veículo possui uma parada.",
-                    })
+                context['error'] = True
+                context['error_mensagem'] = "Veículo não esta cadastrado."
+                context['acao'] = "Realizar notificação via talão!"
+                
+        return render(request, self.template_name, context=context)
             
 
 class VenderCreditos(View):
-    retorno = 'vender.html'
+    template_name = 'vender.html'
     
     def get(self, request):
         form_class = FormCompraCreditos(user=request.user)
-        return render(request, self.retorno, {
-            "form": form_class,
-        })
+        return render(request, self.template_name, { "form": form_class, })
 
     def post(self, request):
+        context = {}
         placa = request.POST.get('placa')
         hora = request.POST.get('horarios')
         hora_selecionada = HorasEstacionar.objects.get(id=hora)
         form_class = FormCompraCreditos(user=request.user)
+        context['form'] = form_class
 
         # CRIA VEICULO DA VENDA DO FUNCIONARIO
         veiculo = Veiculo(placa=placa, apelido='venda funcionario', ativo=True,user=None)
@@ -93,16 +97,12 @@ class VenderCreditos(View):
             veiculo.save()
             parada.save()
             venda.save()
-        except:
-            logger.error(f'Erro gerar Parada - Placa do Veiculo ({placa}) - Id Hora ({hora}) - Funcionario {request.user.id}')
-            return render(request, self.retorno, {
-                "form": form_class,
-                "error": "True",
-                "error_mensagem": "Algo aconteceu de errado! Por gentileza tentar novamente mais tarde.",
-            })
+            context['sucesso'] = True
+            context['sucesso_mensagem'] = "Compra de créditos realizada com sucesso."
 
-        return render(request, self.retorno, {
-            "form": form_class,
-            "sucesso": "True",
-            "sucesso_mensagem": "Compra de créditos realizada com sucesso.",
-        })
+        except:
+            context['error'] = True
+            context['error_mensagem'] = "Algo aconteceu de errado! Por gentileza tentar novamente mais tarde."
+            logger.error(f'Erro gerar Parada - Placa do Veiculo ({placa}) - Id Hora ({hora}) - Funcionario {request.user.id}')
+
+        return render(request, self.retorno, context=context)
